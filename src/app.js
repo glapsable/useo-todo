ac4c4024b1a6edbcc8675edc5af55fa95dec2a00
Glapson <glapsable@gmail.com>
2019-01-24 15:12:18 +0100

    add normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UseoTodo from './components/UseoTodo';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<UseoTodo />, document.getElementById('app'));
