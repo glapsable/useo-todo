074d174e67c55e8740666c711a241ab012694dcb
Glapson <glapsable@gmail.com>
2019-01-24 15:55:43 +0100

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="content-container">
+      <div className="footer__content">
+        <h3>Useo</h3>
+        <p>f</p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/components/UseoTodo.js
+++ src/components/UseoTodo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const UseoTodo = () => (
   <div>
@@ -7,6 +8,7 @@ const UseoTodo = () => (
     <main className="content-container">
       <p>ale fajna ta apka</p>
     </main>
+    <Footer />
   </div>
 );
 
