2b2fb983ee51ffa3450f6d7c50a34552a348a9be
Glapson <glapsable@gmail.com>
2019-01-24 21:15:51 +0100

    add TodoListHeader component

--- src/components/TodoListHeader.js
+++ src/components/TodoListHeader.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 const TodoListHeader = () => (
-  <div className="Rtable Rtable--4cols Rtable--collapse">
-
-    <div className="Rtable-cell Rtable-cell--head"><h3>Eddard Stark</h3></div>
-    <div className="Rtable-cell">Has a sword named Ice</div>
-    <div className="Rtable-cell">No direwolf</div>
-    <div className="Rtable-cell Rtable-cell--foot"><strong>Lord of Winterfell</strong></div>
-
-    <div className="Rtable-cell Rtable-cell--head"><h3>Jon Snow</h3></div>
-    <div className="Rtable-cell">Has a sword named Longclaw</div>
-    <div className="Rtable-cell">Direwolf: Ghost</div>
-    <div className="Rtable-cell Rtable-cell--foot"><strong>Knows nothing</strong></div>
-
+  <div>
+    <div className="table-row table-row--header">
+      <div className="table-row__cell table-row__cell--1">x</div>
+      <div className="table-row__cell table-row__cell--2">Content</div>
+      <div className="table-row__cell table-row__cell--3">Deadline</div>
+      <div className="table-row__cell table-row__cell--4">Actions</div>
+    </div>
   </div>
 );
 
