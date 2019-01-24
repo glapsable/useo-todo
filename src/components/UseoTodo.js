c6d53529ff6230e2c482a0052c4cab12fe448842
Glapson <glapsable@gmail.com>
2019-01-24 14:23:06 +0100

    setup eslint

--- src/components/UseoTodo.js
+++ src/components/UseoTodo.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class UseoTodo extends React.Component {
-    state = {
-        notes: []
-    };
-
-    render() {
-        return(
-            <div>
-                <p>ale fajna ta apka</p>
-            </div>
-        );
-    }
-}
+const UseoTodo = () => (
+  <div>
+    <p>ale fajna ta apka</p>
+  </div>
+);
 
 export default UseoTodo;
