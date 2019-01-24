25879f152fbedbdfc12909d3f00a3e3220e24802
Glapson <glapsable@gmail.com>
2019-01-24 11:55:29 +0100

    add scss handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UseoTodo from './components/UseoTodo';
+import './styles/styles.scss';
 
 ReactDOM.render(<UseoTodo />, document.getElementById('app'));
