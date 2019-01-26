6389e0181a3158f8e4fa87bc220edf3c7b42b48a
Glapson <glapsable@gmail.com>
2019-01-26 12:52:27 +0100

    setup redux part 1

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: './src/playground/redux-notes.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
   module: {
     rules: [{
-      loader: ['babel-loader', 'eslint-loader'],
+      // loader: ['babel-loader', 'eslint-loader'],
+      loader: ['babel-loader'],
       test: /\.js$/,
       exclude: /node_modules/,
     }, {
